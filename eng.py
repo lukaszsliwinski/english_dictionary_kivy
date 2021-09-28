347ffc8160913e43b3db60658715deb66727c96a
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2021-09-28 22:13:25 +0200

    Add function to check the answer and count number of correct answers.

--- eng.py
+++ eng.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.properties import StringProperty
+# from kivy.clock import mainthread
 
 import csv, random
 
@@ -20,13 +21,12 @@ with open('dictionary.csv', mode='r', encoding='utf-8') as infile:
 
 # Choose random polish words to translate and save to list
 pl_words = []
-i=0
+i = 0
 while i < 3:
     random_word = random.choice(list(dictionary))
     if random_word not in pl_words:
         pl_words.append(random_word)
-        i+=1
-
+        i += 1
 
 
 
@@ -38,17 +38,43 @@ class Card(Screen):
         self.word = pl_words[self.id]
 
     def check(self):
-        input = self.ids["input"+str(self.id+1)].text
-        self.ids["input"+str(self.id+1)].readonly = True
-        if input in list(dictionary[self.word]) or input == dictionary[self.word]:
-            print("correct")
-            print(list(dictionary[self.word]))
+        input = self.ids[f'input{str(self.id+1)}'].text
+        correct_counter = int(self.ids[f'result{str(self.id+1)}'].text)
+        self.ids[f'input{str(self.id+1)}'].readonly = True
+
+        answer = ''
+        
+        if len(dictionary[self.word]) == 1:
+            answer = f'{dictionary[self.word][0]}'
         else:
-            print("incorrect")
-            print(list(dictionary[self.word]))
+            for translation in dictionary[self.word]:
+                answer += f'{str(i+1)}. {translation}'
+                answer += '\n'
+        if input in list(dictionary[self.word]) or input == dictionary[self.word]:
+            self.ids[f'answer{str(self.id+1)}'].text = f'correct\n{answer}'
+            correct_counter += 1
+            self.ids[f'result{str(self.id+1)}'].text = f'{correct_counter}'
+        else:
+            self.ids[f'answer{str(self.id+1)}'].text = f'incorrect\n{answer}'
 
-
-
+    def pass_result(self):
+        try:
+            self.manager.get_screen(f'card{str(self.id+2)}').ids[f'result{str(self.id+2)}'].text \
+            = self.manager.get_screen(f'card{str(self.id+1)}').ids[f'result{str(self.id+1)}'].text
+        except:
+            pass
+            # W tym miejscu dodać przesłanie liczby poprawnych odpowiedzi na kartę kończącą program
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# UWAGA! We wszystkich funkcjach ze zmiennymi po self.id należy dodać warunki brzegowe
+# Możliwe błędy przy ostatniej karcie
 
 class Card1(Card):
     id = 0
